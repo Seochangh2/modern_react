8e1c6be8db146498b9dc22aef170e9d718bc8654
Seochangh2 <nomm89@naver.com>
2022-07-13 19:28:05 +0900

    UserList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef, useState } from "react";
+import UserList from "./Component/UserList";
 function App() {
+  const [users, setUsers] = useState([
+    { id: 1, username: "changhee", height: "178" },
+    { id: 2, username: "haeju", height: "160" },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input></input>
+      <div>
+        <UserList users={users}></UserList>
+      </div>
     </div>
   );
 }
