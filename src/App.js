644d3789b6f1ef13476a4ee4e2b001add2307bf7
Seochangh2 <nomm89@naver.com>
2022-07-14 16:10:31 +0900

    CreateButton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { useRef, useState } from "react";
 import UserList from "./Component/UserList";
+import UserManager from "./Component/UserManager";
+
 function App() {
+  const onInputChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const onCreate = () => {
+    setUsers(
+      users.concat({ id: next_id.current, username: username, height: height })
+    );
+    next_id.current += 1;
+  };
   const [users, setUsers] = useState([
     { id: 1, username: "changhee", height: "178" },
     { id: 2, username: "haeju", height: "160" },
   ]);
-
+  const [inputs, setInputs] = useState({ username: "", height: "" });
+  const { username, height } = inputs;
+  const next_id = useRef(3);
   return (
     <div>
-      <input></input>
+      <UserManager
+        username={username}
+        height={height}
+        onInputChange={onInputChange}
+        onCreate={onCreate}
+      ></UserManager>
       <div>
         <UserList users={users}></UserList>
       </div>
