9b45a11f993fa80131c8fa809b39ca01b0012917
Seochangh2 <nomm89@naver.com>
2022-07-14 21:27:40 +0900

    optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import UserList from "./Component/UserList";
 import UserManager from "./Component/UserManager";
 
@@ -11,18 +11,35 @@ function App() {
     });
   };
   const onCreate = () => {
-    setUsers(
+    setUsers((users) =>
       users.concat({ id: next_id.current, username: username, height: height })
     );
     next_id.current += 1;
   };
+  const onRemove = (id) => {
+    setUsers((users) => users.filter((user) => user.id != id));
+  };
+  const onToggle = (id) => {
+    setUsers(
+      users.map(
+        (users) => (user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+    //setUsers 를 함수형 업데이트 함으로서 리렌더링 최적화
+  };
+  const countActiveUsers = (users) => {
+    return users.filter((user) => user.active).length;
+  };
   const [users, setUsers] = useState([
-    { id: 1, username: "changhee", height: "178" },
-    { id: 2, username: "haeju", height: "160" },
+    { id: 1, username: "changhee", height: "178", active: true },
+    { id: 2, username: "haeju", height: "160", active: false },
   ]);
   const [inputs, setInputs] = useState({ username: "", height: "" });
   const { username, height } = inputs;
   const next_id = useRef(3);
+  const active_count = useMemo(() => countActiveUsers(users), [users]);
+  //useMemo : deps에 들어있는 내용이 바뀔때에만 해당함수연산( 불필요햔 리렌더링 X)
   return (
     <div>
       <UserManager
@@ -32,7 +49,12 @@ function App() {
         onCreate={onCreate}
       ></UserManager>
       <div>
-        <UserList users={users}></UserList>
+        <UserList
+          users={users}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        ></UserList>
+        <div>활성 사용자 수 : {active_count}</div>
       </div>
     </div>
   );
--- src/Component/UserList.js
+++ src/Component/UserList.js
@@ -1,19 +1,32 @@
-export default function UserList({ users }) {
+import React, { useMemo, useRef, useState } from "react";
+
+const UserList = ({ users, onRemove, onToggle }) => {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id}></User>
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        ></User>
       ))}
     </div>
   );
-}
+};
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <span>이름 : {user.username}</span>
-      <span>키 : {user.height}</span>
-      <button>삭제</button>
+      <span
+        style={{ cursor: "pointer", color: user.active ? "blue" : "black" }}
+        onClick={() => onToggle(user.id)}
+      >
+        이름 : {user.username}{" "}
+      </span>
+      <span>키 : {user.height} </span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
+export default React.memo(UserList);
